99405989e789cbc0984370209b3045309bc93608
Charlene Jänchen <charlene.jaenchen@code.berlin>
2026-03-25 00:10:07 +0100

    Initial EJS Templating Implementation

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 import fs from 'fs';                                                                // to be able to check for files in 'public'
 
 //Exercise LU
-import data from './data.json' with { type: 'json' };
+import data from './articles.json' with { type: 'json' };
 
 const app = express();
 const PORT = 3000;
@@ -18,9 +18,13 @@ const __dirname = path.dirname(__filename);
 // --- PRE ROUTE MIDDLEWARE --- 
 
 app.use(logger)
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: true}))                                       //to parse data from forms
+
+//evtl. brauche ich nicht mehr
 app.use(express.static(path.join(__dirname, 'public')));                            // googled: absolute path, instead of relative Pfad
 
-app.use(express.urlencoded({extended: true}))                                       //to parse data from forms
+
 
 
 
@@ -32,33 +36,62 @@ app.get('/LU', (req, res) => {
     res.send(data[0].title1)
 })
 
-// static route: redirect Homepaige with /home (must come before dynamic route)
+
+// redirects
 app.get('/home', (req, res) => {
     res.redirect('/')
 })
 
-// static route: redirect Homepaige with /404 
+
+app.get('/index', (req, res) => {
+    res.redirect('/')
+})
+
 app.get('/404', (req, res) => {
     res.redirect('/')
 })
 
 
-//dynmaic route to all subpages
-app.get('/:slug', (req, res) => {
-    const pageName = req.params.slug;                                               // specific value for the placeholder
-    const filePath = path.join(__dirname, 'public', `${pageName}.html`);          
+// static route
+app.get('/', (request, response) => {
+    response.render('index')
+})
 
-    if (fs.existsSync(filePath)) {                                                  // Checks if that file exists
-        res.sendFile(filePath);
-    } else {
-            res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
-        }
+app.get('/profile', (request, response) => {
+    response.render('profile')
+})
+
+app.get('/articles', (request, response) => {
+    const numberOfArticlesAvailable = 3
+    response.render('articles', {numberOfArticlesAvailable: data}, )
+})
+
+app.get('/events', (request, response) => {
+    response.render('events')
+})
+
+app.get('/about', (request, response) => {
+    response.render('about')
+})
+
+app.get('/home', (request, response) => {
+    response.render('404')
+})
+
+
+// contact form route
+app.post('/profile', (request, response) => {
+    console.log('Contact form submission: ', request.body)
+    response.send('<h1>Thank You</h1><p>We will get back to you asap!</p>')
+})
+
+
+// catch-all route
+app.use((req, res) => {
+    res.status(404).render('404');
 });
 
-app.post('/profile', (request, response) => {
-  console.log('Contact form submission: ', request.body)
-  response.send('Thank you for your message. We will be in touch soon.')
-})
+
 
 // --- SERVER ON ---
 
